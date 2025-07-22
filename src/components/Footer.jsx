04e3dd5251dc5266b6e255becac6d3db04f10b57
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-07-22 23:43:00 +0530

    footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <div className="footer-container grid grid-cols-2 sm:grid-cols-3 bg-primary pt-20 pb-10 justify-center items-center md:pl-30 md:pr-30">
+      <div className="footer-container grid grid-cols-1 sm:grid-cols-3 bg-primary pt-20 pb-10 justify-center items-center md:pl-30 md:pr-30">
         <div className="footer-left hidden sm:flex flex-row justify-center gap-10 ">
           <div className="pages flex flex-col gap-5">
             <h2 className="text-primary text-2xl">Pages</h2>
@@ -33,21 +33,56 @@ function Footer() {
 
         <div className="footer-middle flex flex-col gap-5 justify-center items-center">
           <div className="flex flex-row gap-2 ">
-            <img className=" " src="../images/medifit-logo.png" alt="Medifit Logo"/>
+            <img
+              className=" "
+              src="../images/medifit-logo.png"
+              alt="Medifit Logo"
+            />
             <p className="text-6xl text-primary font-medium">Medifit</p>
           </div>
           <div className="text-primary-light-thin text-center w-65">
-            We provides the ultimate solution for your online store and all the healthcare needs.
+            We provides the ultimate solution for your online store and all the
+            healthcare needs.
           </div>
           <div className="flex flex-row gap-5 ">
-            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8"><img src="../images/MedicalStore/facebook.png" alt="" /></span>
-            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8"><img src="../images/MedicalStore/instagram.png" alt="" /></span>
-            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8"><img src="../images/MedicalStore/twitter.png" alt="" /></span>
-            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8"><img src="../images/MedicalStore/youtube.png" alt="" /></span>
+            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8">
+              <img src="../images/MedicalStore/facebook.png" alt="" />
+            </span>
+            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8">
+              <img src="../images/MedicalStore/instagram.png" alt="" />
+            </span>
+            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8">
+              <img src="../images/MedicalStore/twitter.png" alt="" />
+            </span>
+            <span className="flex button-primary rounded-lg justify-center items-center text-center w-8 h-8">
+              <img src="../images/MedicalStore/youtube.png" alt="" />
+            </span>
           </div>
         </div>
 
-        <div className="footer-right"></div>
+        <div className="footer-right w-60 flex flex-row sm:flex-col m-10  gap-10 md:gap-20 md:ml-20 ">
+          <div className="address flex flex-col gap-5">
+            <h2 className="text-primary text-2xl">Address</h2>
+            <div className="flex flex-row gap-2">
+              <img src="../images/location-pin.png" alt="" className="w-fit mb-7 mt-1.5" />
+              <p className="text-primary-light-thin text-start w-60">1640 Parker Rd. Allentown, New Mexico 31134</p>
+            </div>
+          </div>
+
+          <div className="contact flex flex-col gap-5">
+            <h2 className="text-primary text-2xl">Contact</h2>
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-row gap-2">
+                <img src="../images/email.png" alt="" className="mb-2 mt-1.5" />
+                <p className="text-primary-light-thin text-start">SRP@bhai.com</p>
+              </div>
+              <div className="flex flex-row gap-2">
+                <img src="../images/phone-no.png" alt="" className="mb-2 mt-1.5" />
+                <p className="text-primary-light-thin text-start w-40">(209) 555-0404</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
