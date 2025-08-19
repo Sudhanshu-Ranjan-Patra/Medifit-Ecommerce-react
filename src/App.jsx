d8a135602aca0232ce1995b64bfd9f38a84e51b3
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-08-19 23:47:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import React,{ useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import MedicalStore from './components/MedicalStore'
-import Footer from './components/Footer'
-import Home from './components/Home'
-import About from './components/About'
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MedicalStore from "./components/MedicalStore";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
 // import Shop from './components/Shop'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-  <>
-
-    <Navbar/>
-    <Routes> 
-      <Route path="/" element={<MedicalStore/>} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      {/* <Route path="/shop" element={<Shop />} /> */}
-      
-    </Routes>
-    <Footer/>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MedicalStore />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/shop" element={<Shop />} /> */}
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
